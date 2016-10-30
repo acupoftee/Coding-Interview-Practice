b895e5c4d24b382e3ba97005986de7b5ce445d74
BethyDiakabananas <bethyd123@gmail.com>
2016-10-30 18:28:44 -0400

    adding fibbonaci and example1

---
+++ src/net/bethydiakabana/interview/ctci/bigo/Example1.java
@@ -0,0 +1,25 @@
+package net.bethydiakabana.interview.ctci.bigo;
+
+/**
+ * Created by diakabanab on 10/30/2016.
+ */
+class Example1 {
+    /**
+     * finds the sum and product of array elements.
+     * The time compelxity is O(N) because even though there
+     * are two different loops, the time complexity depends on the
+     * size of the array.
+     * @param array
+     */
+    void foo(int[] array) {
+        int sum = 0;
+        int product = 1;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            product *= array[i];
+        }
+        System.out.println(sum + ", " + product);
+    }
+}
---
+++ src/net/bethydiakabana/interview/ctci/bigo/Fibonacci.java
@@ -0,0 +1,19 @@
+package net.bethydiakabana.interview.ctci.bigo;
+
+/**
+ * Created by diakabanab on 10/30/2016.
+ */
+ class Fibonacci {
+    /**
+     * Time complexity: O(2^n) since there are two calls to
+     * fibonacci, which run independently. The space complexity is still O(N)
+     * @param n
+     * @return
+     */
+    int fibonacci(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 1);
+    }
+}
